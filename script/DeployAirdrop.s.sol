7896d34e0a0a8e91a6407c53492bb059dc7cfbac
anotherdev <joseluismanco37@gmail.com>
2025-06-09 11:38:18 -0500

    feat: deploy airdrop on Celo

--- script/DeployAirdrop.s.sol
+++ script/DeployAirdrop.s.sol
@@ -5,20 +5,21 @@ import {Airdrop} from "../src/Airdrop.sol";
 import {MockToken} from "../src/mocks/MockToken.sol";
 
 contract DeployAirdrop is Script {
-    bytes32 private _merkleRoot =
-        0x91673d887a585a027b96f72ced7bdc3c67fd0725fbd74fe1c8e1cba3b6419681;
-    uint256 private _amount = 2500000 * 4 * 1e18;
+    // bytes32 private _merkleRoot =
+    //     0x91673d887a585a027b96f72ced7bdc3c67fd0725fbd74fe1c8e1cba3b6419681;
+    // uint256 private _amount = 2500000 * 4 * 1e18;
 
     function deployAirdrop() public returns (Airdrop, MockToken) {
         vm.startBroadcast();
-        MockToken token = new MockToken();
+        // MockToken token = new MockToken();
         Airdrop airdrop = new Airdrop();
-        airdrop.setMerkleRoot(address(token), _merkleRoot, true);
-        token.mint(airdrop.owner(), _amount);
-        token.approve(address(airdrop), _amount);
+        // airdrop.setMerkleRoot(address(token), _merkleRoot, true);
+        // token.mint(airdrop.owner(), _amount);
+        // token.approve(address(airdrop), _amount);
         console.log("Merkle Airdrop deployed at %s", address(airdrop));
         vm.stopBroadcast();
-        return (airdrop, token);
+        // return (airdrop, token);
+        return (airdrop, MockToken(address(0)));
     }
 
     function run() public {
